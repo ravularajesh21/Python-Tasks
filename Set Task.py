2219b5b0a5713898cd4893348a2a36f8c42293da
ravularajesh21 <84907419+ravularajesh21@users.noreply.github.com>
2021-06-14 23:37:00 +0530

    Add files via upload

---
+++ Set Task.py
@@ -0,0 +1,11 @@
+# What is the difference between the difference and symmetric difference
+
+# Difference()
+a={1,False,'rajesh',4,5,}
+b={4,5,6,7,'mahesh',9}
+c=a.difference(b)
+print('Difference is: ',c)
+
+# Symmetric Difference()
+d=a.symmetric_difference(b)
+print('Symmetric difference is: ',d)
