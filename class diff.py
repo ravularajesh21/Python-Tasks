13b773bffb82c342e1dc8f347976dc2607baaad5
ravularajesh21 <84907419+ravularajesh21@users.noreply.github.com>
2021-07-02 21:48:13 +0530

    Add files via upload

---
+++ class diff.py
@@ -0,0 +1,10 @@
+class A(B):
+    def m1(self):
+       print('shivam is python web devleoper')
+class B():
+    def m(self):
+        print('Rajesh is python web developer')
+
+N = A()
+N.m()
+N.m1()
---
+++ class diff2.py
@@ -0,0 +1,11 @@
+class A(B):
+    def maruthi(self):
+        pass
+        print('Rajesh junior web developer')
+class B():
+    def yamaha(self):
+        print('Shivam is senior web developer')
+        
+G = A()
+G.maruthi()
+G.yamaha()
